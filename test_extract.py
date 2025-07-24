7ccd4510c826461b75f6d574b4a1a6058e096d21
NikhithaK <143378657+Nikhitha35@users.noreply.github.com>
2025-07-24 10:35:18 +0530

    Delete test_extract.py

--- test_extract.py
+++
@@ -1,8 +0,0 @@
-# test_extract.py
-
-from extract_and_chunk import extract_text_chunks
-
-chunks = extract_text_chunks("data/BAJHLIP23020V012223.pdf")
-
-print(f"Extracted {len(chunks)} chunks.")
-print(chunks[0].page_content[:300])
